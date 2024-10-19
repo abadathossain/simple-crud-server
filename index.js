b3e104492b24950d1f749fce34b980cf2829494a
Abadat Hossain <abadat26@gmail.com>
2024-10-19 23:48:24 +0600

    update api partially done

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
+// abadatcmbd
+// jY1D4kXDTX7YwEnY
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const uri = "mongodb+srv://abadatcmbd:jY1D4kXDTX7YwEnY@cluster0.dxzduzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,6 +34,14 @@ app.get('/users',async(req,res)=>{
   const result=await cursor.toArray()
   res.send(result)
 })
+
+
+    app.get('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const user = await userCollection.findOne(query);
+      res.send(user);
+  })
 
 
 app.post('/users', async(req, res)=>{
